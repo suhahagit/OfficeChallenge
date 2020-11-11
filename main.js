0cdb99f8d64e6fd94870258e5f68d27c62f36d76
Your username <suhaha.git@gmail.com>
2020-11-11 15:21:14 +0200

    office challenge completed

--- main.js
+++ main.js
@@ -1,2 +1,67 @@
 //write your code here to make the tests pass
+class Document {
+    constructor(EmployeeName){
+        this.EmployeeName = EmployeeName
+    }
+}
 
+class Employee {
+    constructor(name){
+        this.name = name
+    }
+    
+    work(office){
+        for(let i = 0; i < 10; i++){
+            const doc = new Document(this.name)
+            office.documents.push(doc)
+        }
+    }
+}
+
+class Manager{
+    constructor(name){
+        this.employees = []
+        this.name = name
+    }
+
+    hireEmployee(name){
+        const employee = new Employee(name)
+        this.employees.push(employee)
+    }
+
+    askEmployeesToWork(office){
+        this.employees.forEach(e => e.work(office))
+    }
+}
+
+class Cleaner{
+    constructor(name){
+        this.name = name
+    }
+    clean(){
+        console.log("Clean")
+    }
+}
+
+class Office{
+    constructor(){
+        this.documents = []
+        this.managers = []
+        this.cleaners = []
+    }
+
+    hireCleaner(name){
+        const cleaner = new Cleaner(name)
+        this.cleaners.push(cleaner)
+    }
+
+    hireManager(name){
+        const manager = new Manager(name)
+        this.managers.push(manager)
+    }
+
+    startWorkDay(){
+        this.managers.forEach(m => m.askEmployeesToWork(this))
+        this.cleaners.forEach(c => c.clean())
+    }
+}
